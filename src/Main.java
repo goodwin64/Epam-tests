fed88d690a469f00c7ebdb54b17def67e86d5d59
Max Donchenko <wereya2@gmail.com>
2016-04-10 19:19:40 +0300

    Added task 1 from chapter 1

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
+		taskA1();
+	}
+
+	public static void taskA1() {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter your name and press <Enter>:");
+		String name = scan.next();
+		System.out.printf("Hi, %s! Glad to see you.", name);
 	}
 }
