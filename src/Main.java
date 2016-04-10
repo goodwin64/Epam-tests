03526425711cb95ab68e1601af8b20e2e96d6913
Max Donchenko <wereya2@gmail.com>
2016-04-10 19:41:14 +0300

    Added task 3 solution from chapter 1

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class Main {
 	public static void main(String[] args) {
-		taskA2(args);
+		taskA3();
 	}
 
 	public static void taskA1() {
@@ -17,6 +18,27 @@ public class Main {
 		System.out.println("----- TASK 2 -----");
 		for (int i = args.length - 1; i >= 0; i--) {
 			System.out.print(args[i] + "  ");
+		}
+		System.out.println();
+	}
+
+	public static void taskA3() {
+		System.out.println("----- TASK 3 -----");
+		Scanner scan = new Scanner(System.in);
+		int countWithNewLine, countInSameLine;
+		Random random = new Random();
+		
+		System.out.print("Count of random numbers each in a new line: ");
+		countWithNewLine = scan.nextInt();
+		
+		System.out.print("Count of random numbers each in the same line: ");
+		countInSameLine = scan.nextInt();
+
+		for (int i = 0; i < countWithNewLine; i++) {
+			System.out.println(random.nextInt());
+		}
+		for (int i = 0; i < countInSameLine; i++) {
+			System.out.print(random.nextInt() + " ");
 		}
 		System.out.println();
 	}
