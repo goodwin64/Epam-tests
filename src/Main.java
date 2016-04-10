caae582e33042ee66f64114fe6417687b38a0a2d
Max Donchenko <wereya2@gmail.com>
2016-04-10 19:27:30 +0300

    Added task 2 solution from chapter 1

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,22 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		taskA1();
+		taskA2(args);
 	}
 
 	public static void taskA1() {
+		System.out.println("----- TASK 1 -----");
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Enter your name and press <Enter>:");
 		String name = scan.next();
-		System.out.printf("Hi, %s! Glad to see you.", name);
+		System.out.printf("Hi, %s! Glad to see you.\n", name);
+	}
+
+	public static void taskA2(String[] args) {
+		System.out.println("----- TASK 2 -----");
+		for (int i = args.length - 1; i >= 0; i--) {
+			System.out.print(args[i] + "  ");
+		}
+		System.out.println();
 	}
 }
